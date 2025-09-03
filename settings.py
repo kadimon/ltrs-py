5938b5ab9e5cc02c350963bb5472082d29d80ed8
kadimon <dr.gonzo174@gmail.com>
2025-09-03 19:44:47 +0200

    workflows add samizdat

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ import os
 SESSION = os.environ['SESSION']
 PROXY_URI = os.environ['PROXY_URI']
 MONGO_URI = os.environ['MONGO_URI']
-COVERS_DIR = os.environ['COVERS_PATH']
+COVERS_DIR = os.environ['COVERS_DIR']
 
 DEBUG = os.environ.get('DEBUG')
