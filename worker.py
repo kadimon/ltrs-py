779bbfffbe850398d086e0ea392c49eee45563fb
kadimon <dr.gonzo174@gmail.com>
2025-09-04 10:17:33 +0200

    workflows fix

--- worker.py
+++ worker.py
@@ -39,7 +39,7 @@ def create_task_for_class(wf: BaseLitresPartnersWorkflow) -> Workflow:
         async with async_playwright() as p:
             context = await p.firefox.launch_persistent_context(
                 './profileDir',
-                # proxy={'server': settings.PROXY_URI},
+                proxy={'server': settings.PROXY_URI},
                 headless=False,
                 viewport={'width': 1920, 'height': 1080},
                 timeout=10_000,
