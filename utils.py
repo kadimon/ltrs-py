f1b2c34f5ba13637a1b86ce8043999df74781856
kadimon <dr.gonzo174@gmail.com>
2025-09-16 18:01:47 +0200

    test classmethod

--- utils.py
+++ utils.py
@@ -11,9 +11,7 @@ from playwright.async_api import async_playwright, Page
 from hatchet_sdk import Hatchet, ClientConfig, PushEventOptions, V1TaskStatus
 from PIL import Image
 
-
 from workflow_base import BaseWorkflow
-from interfaces import InputEvent
 import settings
 
 root_logger = logging.getLogger('hatchet')
@@ -26,86 +24,86 @@ hatchet = Hatchet(
     ),
 )
 
-async def run_task(wf: BaseWorkflow, input: BaseModel):
-    async with async_playwright() as p:
-        browser = await p.firefox.connect(settings.DEBUG_PW_SERVER)
+# async def run_task(wf: BaseWorkflow, input: BaseModel):
+#     async with async_playwright() as p:
+#         browser = await p.firefox.connect(settings.DEBUG_PW_SERVER)
 
-        context = await browser.new_context(
-            proxy={'server': settings.PROXY_URI} if wf.proxy_enable else None,
-            viewport={'width': 1920, 'height': 1080},
-        )
+#         context = await browser.new_context(
+#             proxy={'server': settings.PROXY_URI} if wf.proxy_enable else None,
+#             viewport={'width': 1920, 'height': 1080},
+#         )
 
-        page = await context.new_page()
+#         page = await context.new_page()
 
-        instance = wf(
-            name=wf.name,
-            event=wf.event,
-            site=wf.site,
-            input=wf.input,
-            output=wf.output,
-        )
-        result = await instance.task(input, page)
+#         instance = wf(
+#             name=wf.name,
+#             event=wf.event,
+#             site=wf.site,
+#             input=wf.input,
+#             output=wf.output,
+#         )
+#         result = await instance.task(input, page)
 
-        await context.close()
-        await browser.close()
+#         await context.close()
+#         await browser.close()
 
-        pp(result.model_dump())
+#         pp(result.model_dump())
 
-def run_task_sync(wf: BaseWorkflow, input: BaseModel):
-    if settings.DEBUG:
-        asyncio.run(run_task(wf, input))
+# def run_task_sync(wf: BaseWorkflow, input: BaseModel):
+#     if settings.DEBUG:
+#         asyncio.run(run_task(wf, input))
 
 
-async def set_task(input: InputEvent) -> bool:
-    if settings.DEBUG:
-        return False
+# async def set_task(input: InputEvent) -> bool:
+#     if settings.DEBUG:
+#         return False
 
-    hash = hashlib.md5(f'{input.event}{input.url}'.encode()).hexdigest()
-    if await not_dupe(hash, input.dedupe_hours):
-        await hatchet.event.aio_push(
-            input.event,
-            {
-                'url': input.url,
-                'site': input.site,
-            },
-            options=PushEventOptions(
-                additional_metadata={
-                    'customer': input.customer,
-                    'site': input.site,
-                    'url': input.url,
-                    'hash': hash,
-                }
-            )
-        )
-        return True
-    else:
-        return False
+#     hash = hashlib.md5(f'{input.event}{input.url}'.encode()).hexdigest()
+#     if await not_dupe(hash, input.dedupe_hours):
+#         await hatchet.event.aio_push(
+#             input.event,
+#             {
+#                 'url': input.url,
+#                 'site': input.site,
+#             },
+#             options=PushEventOptions(
+#                 additional_metadata={
+#                     'customer': input.customer,
+#                     'site': input.site,
+#                     'url': input.url,
+#                     'hash': hash,
+#                 }
+#             )
+#         )
+#         return True
+#     else:
+#         return False
 
-def set_task_sync(input: InputEvent):
-    if settings.RUN:
-        asyncio.run(set_task(input))
+# def set_task_sync(input: InputEvent):
+#     if settings.RUN:
+#         asyncio.run(set_task(input))
 
-async def not_dupe(hash: str, hours: int) -> bool:
-    runs_list = await hatchet.runs.aio_list_with_pagination(
-        since=datetime.now(timezone.utc) - timedelta(hours=hours),
-        additional_metadata={
-            'hash': hash,
-        },
-        statuses=[
-            V1TaskStatus.RUNNING,
-            V1TaskStatus.QUEUED,
-            V1TaskStatus.COMPLETED,
-        ],
-        limit=1,
-        # only_tasks=True,
-    )
-    # for t in runs_list:
-    #     print(t.additional_metadata)
+# async def not_dupe(hash: str, hours: int) -> bool:
+#     runs_list = await hatchet.runs.aio_list_with_pagination(
+#         since=datetime.now(timezone.utc) - timedelta(hours=hours),
+#         additional_metadata={
+#             'hash': hash,
+#         },
+#         statuses=[
+#             V1TaskStatus.RUNNING,
+#             V1TaskStatus.QUEUED,
+#             V1TaskStatus.COMPLETED,
+#         ],
+#         limit=1,
+#         # only_tasks=True,
+#     )
+#     # for t in runs_list:
+#     #     print(t.additional_metadata)
 
-    if runs_list:
-        return False
-    else:
-        return True
+#     if runs_list:
+#         return False
+#     else:
+#         return True
 
 async def save_cover(page: Page, cover_url: str, timeout: int = 10_000) -> str | None:
     page_url = page.url
