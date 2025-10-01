3229d548ab597956951b1880f1f620bbfbcc642d
kadimon <dr.gonzo174@gmail.com>
2025-10-01 16:47:20 +0200

    add capmonster extention

--- worker.py
+++ worker.py
@@ -69,13 +69,9 @@ def create_task_for_class(wf: BaseLitresPartnersWorkflow) -> Workflow:
                 headless=False,
                 viewport={'width': 1920, 'height': 1080},
                 timeout=10_000,
-                firefox_user_prefs={
-                    'xpinstall.signatures.required': False,
-                    'extensions.autoDisableScopes': 0,
-                    'extensions.enabledScopes': 15,
-                    'extensions.update.enabled': False,  # Отключить авто-обновление
-                    'extensions.update.autoUpdateDefault': False,
-                }
+                args=[
+                    f'--load-extension=/extensions/capmonster'  # Путь к распакованному расширению
+                ]
             )
 
             page = context.pages[0] if context.pages else await context.new_page()
