62dddda98d2708e781ae386c728a08ba4d1eae87
kadimon <dr.gonzo174@gmail.com>
2025-12-16 22:42:03 +0100

    hatchet url del

--- workflow_base.py
+++ workflow_base.py
@@ -26,10 +26,9 @@ root_logger.setLevel(logging.WARNING)
 
 hatchet = Hatchet(
     debug=False,
-    # config=ClientConfig(
-    #     # server_url='http://homeserver:8888',
-    #     logger=root_logger,
-    # ),
+    config=ClientConfig(
+        logger=root_logger,
+    ),
 )
 
 TInput = TypeVar('TInput', bound=interfaces.InputBase)
