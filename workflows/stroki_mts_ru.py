16892f956acc21623bdbfa7a009a8049766d2429
kadimon <dr.gonzo174@gmail.com>
2026-04-20 12:14:07 +0200

    update stroki.mts.ru

--- workflows/stroki_mts_ru.py
+++ workflows/stroki_mts_ru.py
@@ -24,6 +24,7 @@ class StrokiMtsItem(BaseLivelibWorkflow):
     async def task(cls, input: InputLivelibBook, page: Page) -> Output:
         resp = await page.goto(input.url, wait_until='domcontentloaded')
         # await page.wait_for_timeout(3000)
+        await page.wait_for_selector("main")
 
         if resp.status == 404 or page.url == "https://stroki.mts.ru/not-found":
             async with DbSamizdatPrisma() as db:
